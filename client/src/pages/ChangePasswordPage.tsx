e894c22d5240e9f2f0c992b858da2738d0e596c4
John <wheeler6123@gmail.com>
2024-03-18 16:45:10 -0600

    removed unused import from ChangePasswordPage

--- client/src/pages/ChangePasswordPage.tsx
+++ client/src/pages/ChangePasswordPage.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import updatePasswordRequest from "../api/updatePasswordRequest";
 import Header from "../components/Header";
 import UpdatePasswordForm from '../components/Forms/updatePasswordForm';
 
