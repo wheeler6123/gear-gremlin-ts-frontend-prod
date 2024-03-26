0f5f7d66fbdbefd1b38a74be454f1fde4b0fe2fd
John <wheeler6123@gmail.com>
2024-03-26 16:58:39 -0600

    added type assignments to params in TableFooter using an interface

--- client/src/components/Tables/TableFooter.tsx
+++ client/src/components/Tables/TableFooter.tsx
@@ -1,6 +1,13 @@
 import React, { useEffect } from "react";
 
-const TableFooter = ({ range, setPage, page, slice }) => {
+interface TableFooterProps {
+    range: number[];
+    setPage: (page: number) => void;
+    page: number;
+    slice: any[];
+}
+
+const TableFooter: React.FC<TableFooterProps> = ({ range, setPage, page, slice }) => {
     useEffect(() => {
         if(slice.length < 1 && page !== 1) {
             setPage(page - 1);
