d01e9d4d7ef83edb1e311e0906d73817a63d6885
John <wheeler6123@gmail.com>
2024-04-09 10:57:38 -0600

    added login button and styling to header component

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,7 +4,13 @@ const Header = () => {
 
     return (
         <div className="header">
+            <div className="spacer"></div> {/* Spacer div */}
+            <div className="spacer"></div> {/* Spacer div */}
             <div>Gear Gremlin</div>
+            <div className="spacer"></div> {/* Spacer div */}
+            <button className="login-button">
+                <a href="/signin">Login</a>
+            </button>
         </div>
     )
 }
